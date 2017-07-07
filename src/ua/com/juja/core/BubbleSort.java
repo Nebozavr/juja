82de987feb07a95b094e583fd05d3c9f3d512859
Nebozavr <y.bartoshchuk@gmail.com>
2017-07-07 10:20:43 +0300

    Quiz 08

--- src/ua/com/juja/core/BubbleSort.java
+++ src/ua/com/juja/core/BubbleSort.java
@@ -3,20 +3,20 @@ package ua.com.juja.core;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] testArray = new int[] {3,2,5,6,1,9,55};
+        int[] testArray = new int[]{3, 2, 5, 6, 1, 9, 55};
 
         sort(testArray);
 
-        for (int i = 0; i < testArray.length; i++) {
-            System.out.print(testArray[i] + ",");
+        for (int aTestArray : testArray) {
+            System.out.print(aTestArray + ",");
         }
     }
 
     private static void sort(int[] array) {
-        int element = 0;
+        int element;
         for (int i = 0; i < array.length; i++) {
-            for (int j = array.length -1; j > i ; j--) {
-                if (array[j - 1] > array[j]){
+            for (int j = array.length - 1; j > i; j--) {
+                if (array[j - 1] > array[j]) {
                     element = array[j - 1];
                     array[j - 1] = array[j];
                     array[j] = element;
--- src/ua/com/juja/core/Test.java
+++ src/ua/com/juja/core/Test.java
@@ -1,42 +1,43 @@
 package ua.com.juja.core;
 
-/**
- * Created by ybartoschuk on 03.07.2017.
- */
+import java.util.Arrays;
+
 public class Test {
 
 
-
     public static void main(String[] args) {
-       // System.out.println(printPurchases(false,true));
-        int[] array = new int[] {1, 3, 6};
-        int[] array2 = new int[] {4, 5, 2};
+        // System.out.println(printPurchases(false,true));
+        int[] array = new int[]{1, 23, 6};
+     //   int[] array2 = Arrays.sort(array);
+        Arrays.sort(array);
+
+        System.out.println(Arrays.toString(array));
 
 /*
         for (int k: array) {
             System.out.print(k);
         }*/
-        int[] mass = merge(array, array2);
+       /* int[] mass = merge(array, array2);
         System.out.println();
         for (int k: mass) {
-            System.out.print(k);
-        }
+            System.out.print(k);*/
+
     }
 
     public static String printPurchases(boolean hasEggs, boolean hasBread) {
        /*BODY*/
         String s1 = "eggs";
-        String s2  = "bread";
-        String ss = null;
-            if (hasEggs && hasBread) {
-                ss = s1 + "," + s2;
-            } else if (!hasEggs && !hasBread) {
-                ss = "";
-            } else if (!hasEggs && hasBread){
-                ss = s2;
-            } else ss = s1;
+        String s2 = "bread";
+        String ss;
+        if (hasEggs && hasBread) {
+            ss = s1 + "," + s2;
+        } else if (!hasEggs && !hasBread) {
+            ss = "";
+        } else if (!hasEggs) {
+            ss = s2;
+        } else ss = s1;
 
-            return ss;
+        return ss;
 
 
     }
@@ -44,33 +45,45 @@ public class Test {
     public static String getMonthOfYear(int number) {
         /*BODY*/
         String month = null;
-        switch (number){
-            case 1: month = "January";
-                    break;
-            case 2: month = "February";
-                    break;
-            case 3: month = "March";
-                    break;
-            case 4: month = "April";
-                    break;
-            case 5: month = "May";
-                    break;
-            case 6: month = "June";
-                    break;
-            case 7: month = "July";
-                    break;
-            case 8: month = "August";
-                    break;
-            case 9: month = "September";
-                    break;
-            case 10: month = "October";
-                    break;
-            case 11: month = "November";
-                    break;
-            case 12: month = "December";
-                    break;
+        switch (number) {
+            case 1:
+                month = "January";
+                break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;
+            case 4:
+                month = "April";
+                break;
+            case 5:
+                month = "May";
+                break;
+            case 6:
+                month = "June";
+                break;
+            case 7:
+                month = "July";
+                break;
+            case 8:
+                month = "August";
+                break;
+            case 9:
+                month = "September";
+                break;
+            case 10:
+                month = "October";
+                break;
+            case 11:
+                month = "November";
+                break;
+            case 12:
+                month = "December";
+                break;
             default:
-                    break;
+                break;
         }
         return month;
     }
@@ -84,7 +97,7 @@ public class Test {
         }
     }
 
-    public static int [] filterEven(int [] nums){
+    public static int[] filterEven(int[] nums) {
         int[] array = new int[nums.length];
 
         for (int i = 0, k = 0; i < nums.length; i++) {
@@ -102,17 +115,17 @@ public class Test {
         int fstIndex = 0;
         int sndIndex = 0;
 
-        while ( fstIndex < fst.length && sndIndex < snd.length ) {
-            if ( fst[fstIndex] < snd[sndIndex] ) {
+        while (fstIndex < fst.length && sndIndex < snd.length) {
+            if (fst[fstIndex] < snd[sndIndex]) {
                 result[fstIndex + sndIndex] = fst[fstIndex++];
             } else {
                 result[fstIndex + sndIndex] = snd[sndIndex++];
             }
         }
-        while ( fstIndex < fst.length ) {
+        while (fstIndex < fst.length) {
             result[fstIndex + sndIndex] = fst[fstIndex++];
         }
-        while ( sndIndex < snd.length ) {
+        while (sndIndex < snd.length) {
             result[fstIndex + sndIndex] = snd[sndIndex++];
         }
 
