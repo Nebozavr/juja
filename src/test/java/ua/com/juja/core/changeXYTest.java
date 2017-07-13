4a0c05277710dd3be45da702ec07ac8306de65c1
Nebozavr <y.bartoshchuk@gmail.com>
2017-07-13 18:06:41 +0300

    commit

---
+++ src/test/java/ua/com/juja/core/changeXYTest.java
@@ -0,0 +1,16 @@
+package ua.com.juja.core;
+
+
+import org.junit.Test;
+
+public class changeXYTest {
+
+    @Test
+    public void test1(){
+
+    }
+    @Test
+    public void test2(){
+
+    }
+}
