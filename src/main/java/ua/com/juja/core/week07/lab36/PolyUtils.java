fae58e71bb11128b90f237bc64a96d2dcd7d014a
Nebozavr <y.bartoshchuk@gmail.com>
2017-07-27 15:20:35 +0300

    +lab36

---
+++ src/main/java/ua/com/juja/core/week07/lab36/PolyUtils.java
@@ -0,0 +1,27 @@
+package ua.com.juja.core.week07.lab36;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+
+public class PolyUtils {
+    public static void main(String[] args) {
+        BigInteger[] data = new BigInteger[] {new BigInteger("1"), new BigInteger("1")};
+        BigInteger[] data1 = new BigInteger[] {new BigInteger("1"), new BigInteger("1")};
+
+        System.out.println(Arrays.toString(mul(data, data1)));
+    }
+
+    public static BigInteger[] mul(BigInteger[] x, BigInteger[] y) {
+        BigInteger[] result = new BigInteger[x.length + y.length - 1];
+        Arrays.fill(result, BigInteger.ZERO);
+
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < y.length; j++) {
+                result[i + j] = result[i + j].add(x[i].multiply(y[j]));
+            }
+        }
+
+        return result;
+    }
+}
